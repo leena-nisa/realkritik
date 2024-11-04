36eadcd9e369d74d0fdfbaa4d70858b594000653
RyFranco <rymfranco@gmail.com>
2024-11-03 19:34:55 -0800

    adding a view

--- obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
+++ obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1149e85302ef22bf62766b2c5a55da7f6268df90")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+646008a022a3e6562f54e2e947c09d50ec34e134")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
