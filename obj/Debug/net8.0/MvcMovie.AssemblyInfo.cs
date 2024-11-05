5326bfe05c42820c8e93a59503b79d052f985a40
Alanj818 <145750400+Alanj818@users.noreply.github.com>
2024-11-04 18:06:28 -0800

    not pushing

--- obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
+++ obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21d5b9f7b5e96a90eb1d7286192dd2beed586a24")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea73c47962f0b3c3bf55643703ba08b09ea0a02b")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
