238e54fb6b12d580bffded624c40147c33d3aad2
Alanj818 <145750400+Alanj818@users.noreply.github.com>
2024-11-21 00:37:05 -0800

    dont push

--- obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
+++ obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5326bfe05c42820c8e93a59503b79d052f985a40")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cef6817e9f8f9b908d52f531b1ca957ea97e26d9")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
