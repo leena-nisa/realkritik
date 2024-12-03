ac4d403625efb5d786653be3cdf56afdc4e726e1
Lawrence <108031297+ltahmasian@users.noreply.github.com>
2024-12-03 13:49:38 -0800

    Refactored/Refined mainPage.css
    Refined the page with the Movie Catalog to match the UI design, color, and theme of Account and Home Sign-in Page by refactoring HTML/CSS

--- obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
+++ obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3ca3cf6debac9508c2694e863a09246a9f5bc6f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69c59c3f621e07eb015a5c7295143cafe249ff5d")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
