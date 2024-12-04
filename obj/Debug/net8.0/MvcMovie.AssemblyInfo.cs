225f16c96a9f523bf40cbc7e4e86f84cc6fbfd24
RyFranco <rymfranco@gmail.com>
2024-12-04 04:54:56 -0800

    last pushesdasjn

--- obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
+++ obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5fd8e73435f84a1616ee3e5bfaa40980975e1ed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45c6fe22f9a8a0fce14b6e2adb0550bd2ec361a6")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
